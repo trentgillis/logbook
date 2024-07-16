0dec6a3edab47c816d6b5aec73d5c3eb0b128d67
Trentin Gillis <trentin.gillis@gmail.com>
2024-07-15 22:25:48 -0600

    pre dev setup stuff

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import '@fontsource/lato/300.css';
+import '@fontsource/lato/400.css';
+import '@fontsource/lato/700.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from '@/App';
+import GlobalStyles from '@/styles/global';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
