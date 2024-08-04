fbae3f2e4c3d963de5437298e837c23aa8850e21
Trentin Gillis <trentin.gillis@gmail.com>
2024-08-04 08:58:02 -0600

    remove styled components in favor of css modules

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 import '@fontsource/lato/300.css';
 import '@fontsource/lato/400.css';
 import '@fontsource/lato/700.css';
+import '@/styles/reset.css';
+import '@/styles/global.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from '@/App';
-import GlobalStyles from '@/styles/global';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <GlobalStyles />
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
